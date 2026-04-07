e8097161e1c15442588b2952fa3f7a7dcb05c960
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-04-07 14:56:17 -0300

    .

---
+++ Aulas2/exercicio3.php
@@ -0,0 +1,11 @@
+<?php
+$produtos = [
+["nome" => "Biblia", "preco" => "29.80"],
+["nome" => "Dados de tabuleiro", "preco" => "6.00"],
+["nome" => "Lampada LED", "preco" => "66.07"]
+
+];
+foreach ($produtos as $produto) {
+echo "Produto: { <br>";
+}
+?>
