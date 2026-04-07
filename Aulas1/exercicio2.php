ab41c6b0d36db59500645b5f165fb74a9afef294
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-04-07 08:41:50 -0300

    2

---
+++ Aulas1/exercicio2.php
@@ -0,0 +1,11 @@
+<?php
+
+$produto = [
+    'nome' => 'Coxinha de catupiry',
+    'preco' => 15.09,
+    'quantidade' => 3
+];
+
+echo "Nome do produto: " . $produto['nome'] . "<br>";
+echo "Preço: R$ " . $produto['preco'];
+?>
