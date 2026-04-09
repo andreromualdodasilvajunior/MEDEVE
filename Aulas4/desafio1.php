66f805eedbc89f0eb377f04721713031c8d5bb63
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-04-09 09:21:47 -0300

    4

---
+++ Aulas4/desafio1.php
@@ -0,0 +1,4 @@
+<?php
+echo "Bem vindo, " .
+$_SESSION["usuario"];
+?>
---
+++ Aulas4/exercicio3.php
@@ -0,0 +1,6 @@
+<?php
+$usuarios = [ 
+    ["usuario" => "andre123", "senha" => "8630", "perfil" => "aluno"], 
+    ["usuario" => "marcos", "senha" => "9245", "perfil" => "proprietario"] 
+];
+?>
